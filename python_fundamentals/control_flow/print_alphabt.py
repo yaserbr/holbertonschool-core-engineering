b2d5e76a54e6dbd020b4ea564372ae12dad5d9e3
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-19 20:55:04 +0300

    2

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
-print("abcdfghijklmnoprstuvwxyz")
+
+for letter in range(97, 123):
+    if letter == 101 or letter == 113:
+        continue
+    print("{:c}".format(letter), end="")
