6e82d6420989c22f9e936e4ead2db4a07d394a80
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-19 23:42:56 +0300

    1

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+
 def uppercase(str):
-    return str.upper()
+    result = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
