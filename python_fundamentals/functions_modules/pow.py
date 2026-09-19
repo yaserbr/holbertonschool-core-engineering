bd544f874b73bc2434196d2bbffe51b831257d13
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-19 23:50:31 +0300

    1

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 def pow(a, b):
     result = 1
-    for i in range(b):
+    exponent = b if b >= 0 else -b
+    for i in range(exponent):
         result = result * a
+    if b < 0:
+        result = 1 / result
     return result
