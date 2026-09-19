d3210e8418e5378a8258013fc8898cfb9424525a
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-19 21:49:32 +0300

    1

---
+++ python_fundamentals/control_flow/print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+for intger in range(100):
+    print("{:02d}".format(intger), end=", ")
--- python_fundamentals/control_flow/print_hexa.py
+++ python_fundamentals/control_flow/print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 text = ""
-for i in range(99):
+for i in range(100):
     text += "{} = {}\n".format(i, hex(i))
 print(text, end="")
