279ccc0d6590efb194df7a9d66bc915e5dc8148d
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-21 18:44:04 +0300

    new Day

--- python_fundamentals/exception_handling/main.py
+++ python_fundamentals/exception_handling/main.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-safe_print_integer = __import__('safe_print_integer').safe_print_integer
+safe_print_list_integers = __import__('safe_print_list_integers').safe_print_list_integers
 
-safe_print_integer(89)
-safe_print_integer("School")
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
