e62149fb9e7bb63ee0dfa4c6d8724135c48051a5
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-21 19:26:06 +0300

    2

--- python_oop/classes_and_object_model/0-square.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Module that defines a Square class."""
-
-
-class Square:
-    """Represents a square."""
---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class."""
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
