ba99e3a7eae5fb6da8748df67af3ac6084f32f3d
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-22 17:14:00 +0300

    5

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -17,5 +17,18 @@ class Square:
             raise ValueError("size must be >= 0")
         self.size = size
 
+    @property
+    def size(self):
+        """Getter for the size attribute."""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
         return self.size * self.size
