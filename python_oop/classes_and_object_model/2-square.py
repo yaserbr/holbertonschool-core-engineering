bf9514c48f9ba01da9a11c9c6edf2e420ffc9079
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-22 16:59:19 +0300

    3

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -11,4 +11,8 @@ class Square:
         Args:
             size (int): The size of the square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
