117d4d6665190f9358f973b0e97bf8afa083053e
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-25 20:47:01 +0300

    2

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be a non-negative integer")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -39,5 +39,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be a non-negative integer")
+            raise ValueError("height must be >= 0")
         self.__height = value
