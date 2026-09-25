7eeca002afc307730c3a5d1f1ec24d2e12c398f7
yaser8541 <443017396@sm.imamu.edu.sa>
2026-09-25 21:27:08 +0300

    4.2

--- python_oop/inheritance/0-polymorphism_demo.py
+++ python_oop/inheritance/0-polymorphism_demo.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 class Animal:
+    """Represents an animal."""
     def speak(self):
+        """Returns a generic sound for the animal."""
         return "Some sound"
 
 
 class Dog(Animal):
+    """Represents a dog, which is a subclass of Animal."""
     def speak(self):
+        """Returns the sound a dog makes."""
         return "Woof"
 
 
 class Cat(Animal):
+    """Represents a cat, which is a subclass of Animal."""
     def speak(self):
         return "Meow"
 
